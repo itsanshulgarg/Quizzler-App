1dd208ef2c65fe1424c6c3c16225539ba018ca74
itsanshulgarg <04anshulgarg@gmail.com>
2019-09-01 17:43:30 +0530

    Second commit

--- Quizzler/QuestionBank.swift
+++ Quizzler/QuestionBank.swift
@@ -9,13 +9,10 @@ import Foundation
 class QuestionBank {
     var list = [Question]()
     init(){
-        // Creating a quiz item and appending it to the list
-        let item = Question(text: "Valentine\'s day is banned in Saudi Arabia.", correctAnswer: true)
         
-        // Add the Question to the list of questions
-        list.append(item)
+        list.append( Question(text: "Valentine\'s day is banned in Saudi Arabia.", correctAnswer: true)
+)
         
-        // skipping one step and just creating the quiz item inside the append function
         list.append(Question(text: "A slug\'s blood is green.", correctAnswer: true))
         
         list.append(Question(text: "Approximately one quarter of human bones are in the feet.", correctAnswer: true))
